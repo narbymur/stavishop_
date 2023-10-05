d0d3c36a47dbbe148957bda07cb70d81ac909581
annas <annastavi164164@yandex.ru>
2023-10-05 18:18:56 +0300

    Диплом

---
+++ products/tables/Products.sql
@@ -0,0 +1,6 @@
+nm_id BIGINT
+sku_id BIGINT --XXS,XS,S,M,L,XL,XXL
+gender SMALLINT -- 1, 2
+season CHAR(3) -- SUM, WIN, AUT, SPR, ALL
+product_type SMALLINT --dictionary.productsType -- детское, взрослое, тинейджер и т.д
+product_category SMALLINT --dictionry.productsCategory -- штаны, футболка, свитер, рубашка, платье, пижама.
