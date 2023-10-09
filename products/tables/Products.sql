114aaa28e98c7d3df91d2fa737a336d41c90156f
annas <annastavi164164@yandex.ru>
2023-10-09 12:30:22 +0300

    upd

--- products/tables/Products.sql
+++ products/tables/Products.sql
@@ -3,8 +3,7 @@ CREATE TABLE IF NOT EXISTS products.products
     nm_id            BIGINT      NOT NULL,
     product_type     BIGSERIAL   NOT NULL,
     product_category BIGSERIAL   NOT NULL,
-    sku_id           BIGINT      NOT NULL
-        CONSTRAINT pk_sku PRIMARY KEY,
+    sku_id           BIGINT      NOT NULL,
     gender           SMALLINT    NOT NULL,
     season           CHAR(3)     NOT NULL,
     color            VARCHAR(30) NOT NULL,
