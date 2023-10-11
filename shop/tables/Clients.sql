eb245cf6b63d06720a883815bcf3c2fe3727665b
annas <annastavi164164@yandex.ru>
2023-10-11 17:50:10 +0300

    Добавила уникальность телефона у клиента

--- shop/tables/Clients.sql
+++ shop/tables/Clients.sql
@@ -5,7 +5,8 @@ CREATE TABLE IF NOT EXISTS shop.clients
     name        VARCHAR(300) NOT NULL,
     birth_date  DATE         NOT NULL,
     gender      CHAR(3)      NOT NULL,
-    phone       VARCHAR(11)  NOT NULL,
+    phone       VARCHAR(11)  NOT NULL
+        CONSTRAINT uq_clients_phone UNIQUE,
     ch_staff_id INT          NOT NULL,
     ch_dt       TIMESTAMPTZ  NOT NULL
 );
